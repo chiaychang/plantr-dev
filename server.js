1775ff84bf71e09c3780048a46617715ae577477
Joy Chang <chiaychang@users.noreply.github.com>
2017-07-27 13:38:42 -0500

    fix db url

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 // MongoDB configuration (Change this URL to your own DB)
 // MONGODB_URI: MONGODB_URI: mongodb://heroku_t89rtnkv:6ueev1035ohcarava93trgdf6c@ds133311.mlab.com:33311/heroku_t89rtnkv
 //local: mongodb://localhost:27017/plantsdb
-mongoose.connect("MONGODB_URI: mongodb://heroku_t89rtnkv:6ueev1035ohcarava93trgdf6c@ds133311.mlab.com:33311/heroku_t89rtnkv", { useMongoClient: true });
+mongoose.connect("mongodb://heroku_t89rtnkv:6ueev1035ohcarava93trgdf6c@ds133311.mlab.com:33311/heroku_t89rtnkv", { useMongoClient: true });
 var db = mongoose.connection;
 
 db.on("error", function(err) {
